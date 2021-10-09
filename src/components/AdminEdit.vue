99223b93e26e29bca08a030a84756974ec24dde9
Wi_armonia <leegosoo11@gmail.com>
2021-10-10 00:53:33 +0900

    admin, staff tab

--- src/components/AdminEdit.vue
+++ src/components/AdminEdit.vue
@@ -15,48 +15,146 @@
           </v-btn>
         </v-toolbar>
 
-        <v-data-table 
-          :headers="headers" 
-          :items="items" 
-          hide-default-footer 
-          disable-pagination
-          disable-sort
-        >
-          <template v-slot:item.name="{ item }">
-            <label>{{ item.name }}</label>
-          </template>
-          <template v-slot:item.comment="{ item }">
-            <v-text-field
-              height="30"
-              class="mx-2 mt-4"
-              v-model="item.comment"
-              label="案件"
-              auto-grow
-              rows=1
-            ></v-text-field>
-          </template>
-          <template v-slot:item.hour_salary="{ item }">
-            <v-text-field
-              height="30"
-              class="mx-2 mt-4"
-              v-model="item.hour_salary"
-              label="時給"
-            ></v-text-field>
-          </template>
-          <template v-slot:item.day_salary="{ item }">
-            <v-text-field
-              height="30"
-              class="mx-2 mt-4"
-              v-model="item.day_salary"
-              label="日給"
-            ></v-text-field>
-          </template>
-          <template v-slot:item.action="{ item }">
-            <v-btn class="error ml-1" icon color="white" @click="remove(item)">
-              <v-icon>delete</v-icon>
-            </v-btn>
-          </template>
-        </v-data-table>
+        <v-tabs v-model="tab" grow>
+          <v-tab v-for="item in tab_item" :key="item">
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+          <v-tab-item key="管理者">
+            <v-data-table 
+              :headers="headers" 
+              :items="items" 
+              hide-default-footer 
+              disable-pagination
+              disable-sort
+            >
+              <template v-slot:item.agenda="{ item }">
+                <v-text-field
+                  height="40"
+                  class="mx-2 mt-4"
+                  v-model="item.agenda"
+                  label="案件"
+                  rows=1
+                ></v-text-field>
+              </template>
+              <template v-slot:item.total_time="{ item }">
+                <v-text-field
+                  height="40"
+                  class="mx-2 mt-4"
+                  v-model="item.total_time"
+                ></v-text-field>
+              </template>
+              <template v-slot:item.admin_hour_salary="{ item }">
+                <v-text-field
+                  v-if="item.total_time == ''"
+                  height="40"
+                  class="mx-2 mt-4"
+                  :value="item.admin_hour_salary = ''"
+                  label="時給"
+                  :disabled="item.total_time == ''"
+                ></v-text-field>
+                <v-text-field
+                  v-else
+                  height="40"
+                  class="mx-2 mt-4"
+                  v-model="item.admin_hour_salary"
+                  label="時給"
+                ></v-text-field>
+              </template>
+              <template v-slot:item.admin_day_salary="{ item }">
+                <v-text-field
+                  v-if="item.admin_hour_salary"
+                  height="40"
+                  class="mx-2 mt-4"
+                  :value="item.admin_day_salary = get_admin_day_salary(item)"
+                  label="日給"
+                  :disabled="item.total_time != ''"
+                ></v-text-field>
+                <v-text-field
+                  v-else
+                  height="40"
+                  class="mx-2 mt-4"
+                  v-model="item.admin_day_salary"
+                  label="日給"
+                ></v-text-field>
+              </template>
+              <template v-slot:item.action="{ item }">
+                <v-btn class="error" icon color="white" @click="remove(item)">
+                  <v-icon>delete</v-icon>
+                </v-btn>
+              </template>
+            </v-data-table>
+          </v-tab-item>
+
+          <v-tab-item key="スタッフ">
+            <v-data-table 
+              :headers="header" 
+              :items="items" 
+              hide-default-footer 
+              disable-pagination
+              disable-sort
+            >
+              <template v-slot:item.start_time="{ item }">
+                <v-text-field
+                  height="40"
+                  class="mx-2 mt-4"
+                  v-model="item.start_time"
+                  label="出勤時間"
+                  placeholder="0900"
+                  :rules="rules"
+                  counter=4
+                  :disabled="item.total_time == ''"
+                ></v-text-field>
+              </template>
+              <template v-slot:item.end_time="{ item }">
+                <v-text-field
+                  height="40"
+                  class="mx-2 mt-4"
+                  v-model="item.end_time"
+                  label="退勤時間"
+                  placeholder="1800"
+                  :rules="rules"
+                  counter=4
+                  :disabled="item.total_time == ''"
+                ></v-text-field>
+              </template>
+              <template v-slot:item.total_time="{ item }">
+                <v-text-field
+                  height="40"
+                  class="mx-2 mt-4"
+                  v-model="item.total_time"
+                ></v-text-field>
+              </template>
+              <template v-slot:item.staff_hour_salary="{ item }">
+                <v-text-field
+                  height="40"
+                  class="mx-2 mt-4"
+                  v-model="item.staff_hour_salary"
+                  label="時給"
+                  :disabled="item.total_time == ''"
+                ></v-text-field>
+              </template>
+              <template v-slot:item.staff_day_salary="{ item }">
+                <v-text-field
+                  v-if="item.staff_hour_salary"
+                  height="40"
+                  class="mx-2 mt-4"
+                  :v-model="item.staff_day_salary = get_staff_day_salary(item)"
+                  label="日給"
+                  :disabled="item.total_time != ''"
+                ></v-text-field>
+                <v-text-field
+                  v-else
+                  height="40"
+                  class="mx-2 mt-4"
+                  v-model="item.staff_day_salary"
+                  label="日給"
+                ></v-text-field>
+              </template>
+            </v-data-table>
+          </v-tab-item>
+        </v-tabs-items>
       </v-card>
     </v-container>
   </v-dialog>
@@ -72,24 +170,42 @@ export default {
   ],
   data: () => ({
     headers: [
-      { value:"name", text:"名前", width:"20%", align: 'start'},
-      { value:"comment", text:"案件", width:"25%", align: 'center'},
-      { value:"hour_salary", text:"時給", width:"20%", align: 'center'},
-      { value:"day_salary", text:"日給", width:"20%", align: 'center'},
+      { value:"name", text:"名前", width: "25%", align: 'start'},
+      { value:"agenda", text:"案件", width: "25%", align: 'start'},
+      { value:"total_time", text:"勤労時間", width: "10%", align: 'center'},
+      { value:"admin_hour_salary", text:"時給", width: "10%", align: 'center'},
+      { value:"admin_day_salary", text:"日給", width: "10%", align: 'center'},
       { value:"action", text:"削除", width:"10%", align: 'center'}
     ],
+    header: [
+      { value:"name", text:"名前", width: "25%", align: 'start'},
+      { value:"agenda", text:"案件", width: "25%", align: 'start'},
+      { value:"start_time", text:"出勤時間", width: "10%", align: 'center'},
+      { value:"end_time", text:"退勤時間", width: "10%", align: 'center'},
+      { value:"total_time", text:"勤労時間", width: "10%", align: 'center'},
+      { value:"staff_hour_salary", text:"時給", width: "10%", align: 'center'},
+      { value:"staff_day_salary", text:"日給", width: "10%", align: 'center'}
+    ],
+    tab: null,
+    tab_item: ['管理者', 'スタッフ'],
+    rules: [v => v.length == 4 || v == '' || '4桁入力'],
     edit_date: '',
     dialog: false,
   }),
   created() {
     this.edit_date = this.date.split("-");
     this.edit_date = this.edit_date[0] +"年 "+ this.edit_date[1] +"月 "+ this.edit_date[2] +"日";
-
     this.dialog = true;
   },
   computed: {
   },
   methods: {
+    get_admin_day_salary(item) {
+      return String(item.admin_hour_salary * item.total_time)
+    },
+    get_staff_day_salary(item) {
+      return String(item.staff_hour_salary * item.total_time)
+    },
     remove(item) {
       const index = this.items.findIndex(obj => obj.name == item.name);
       this.items.splice(index, 1);
