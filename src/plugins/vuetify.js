d2e434d8ca78996eb36185475fd1bb148f41bc53
Wi_armonia <leegosoo11@gmail.com>
2021-10-10 17:35:29 +0900

    Unnecessary value import delete

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,14 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
-import '@mdi/font/css/materialdesignicons.css'
+// import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify);
 
 const opts = {
-    icons: {
-      iconfont: 'mdi', // default - only for display purposes
-    },
   }
   
   export default new Vuetify(opts);
