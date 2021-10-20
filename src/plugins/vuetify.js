1c186deaf13ee527f386e0cb82181c4707bb6aa9
seonggon-mediailab-jp <77131899+seonggon-mediailab-jp@users.noreply.github.com>
2021-10-20 17:40:00 +0900

    add fonts

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
-// import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify);
 
 const opts = {
-  }
-  
-  export default new Vuetify(opts);
-  
+  icons: {
+    iconfont: 'mdi', // default - only for display purposes
+  },
+}
+
+export default new Vuetify(opts);
