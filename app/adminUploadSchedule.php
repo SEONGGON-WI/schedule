8de26bffca4f8844fe3d247c6dc14fe4d4e5a892
seonggon-mediailab-jp <77131899+seonggon-mediailab-jp@users.noreply.github.com>
2021-11-05 15:49:09 +0900

    client agenda update schedule client

--- app/adminUploadSchedule.php
+++ app/adminUploadSchedule.php
@@ -1,68 +1,16 @@
 <?php
 $response = json_decode(file_get_contents('php://input'), true);
-$event = $response['event'];
-$start_date = $response['start_date'];
-$end_date = $response['end_date'];
+$client = $response['client'];
 include 'sqlConnect.php';
 try {
-  $rootPath = $_SERVER['DOCUMENT_ROOT'].'/schedule/log/';
-  $time = date('Y/m/d-H:i');
-  $logDate = date('Ym');
-  $path = $rootPath.$logDate.".txt";
-  try {
-    if (!file_exists($path)) {
-      $log = @fopen($path,"a+");
-      @fwrite($log,"time,api,date\n");
-      @fclose($log);
-    }
-    $remoteAddr = $_SERVER['REMOTE_ADDR'];
-    $log = @fopen($path,"a+");
-    @fwrite($log,"$time,'upload',$start_date.' : '.$end_date\n");
-    @fclose($log);
-  } catch(Exception $e) {
-    $logError = true;
-  }
-
   $dbConnect = new mysqlConnect();
-  $data = $dbConnect->getAdmin($start_date, $end_date);
-  $del = "DELETE FROM schedule WHERE";
-  $index = 0;
-  $sql_array = [];
-  if (isset($data)) {
-    foreach ($data as $element) {
-      $duplicate = true;
-      foreach ($event as $values) {
-        if ($element['name'] == $values['name'] && $element['date'] == $values['date']) {
-          $duplicate = false;
-        }
-        $sql_array[$index] = "( '{$values['name']}', '{$values['date']}', '{$values['agenda']}', '{$values['start_time']}', '{$values['end_time']}', '{$values['total_time']}', 
-                              '{$values['staff_hour_salary']}', '{$values['staff_day_salary']}', '{$values['staff_expense']}', 
-                              '{$values['admin_hour_salary']}', '{$values['admin_day_salary']}' )";
-        $index++;
-      }
-      if ($duplicate) {
-        $del = $del." (name = '{$element['name']}' AND date = '{$element['date']}') OR";
-      }
-    }
-    if ($del != "DELETE FROM schedule WHERE") {
-      $del = substr($del, 0, -3);
-      $dbConnect->mysql->query($del);
-    }
-    $sql = "INSERT INTO schedule ( name, date, agenda, start_time, end_time, total_time, staff_hour_salary, staff_day_salary, staff_expense, admin_hour_salary, admin_day_salary ) VALUES";
-    $sub_sql = "ON DUPLICATE KEY UPDATE agenda = VALUES(agenda), start_time = VALUES(start_time), end_time = VALUES(end_time), total_time = VALUES(total_time), 
-              staff_hour_salary = VALUES(staff_hour_salary), staff_day_salary = VALUES(staff_day_salary),  staff_expense = VALUES(staff_expense),
-              admin_hour_salary = VALUES(admin_hour_salary), admin_day_salary = VALUES(admin_day_salary)";
-    $sub_sql_query = implode(', ', $sql_array);
-    $sql = $sql.$sub_sql_query.$sub_sql;
+  foreach ($client as $values) {
+    $sql = "UPDATE schedule SET client = '{$values['client']}' WHERE agenda = '{$values['agenda']}'";
     $dbConnect->mysql->query($sql);
-
-    // $time = date('Y-m-d h:i:s', time());
-    // $time_sql = "UPDATE manager SET access_time = '$time'";
-
-    $result = json_encode(array('status' => 'success' , 'message' => '登録を完了しました。'));
   }
+  $result = json_encode(array('status' => 'success' , 'message' => '登録を完了しました。'));
 } catch(Exception $e) {
-  $result = json_encode(array('error' => 'error' , 'message' => '登録を失敗しました。'));
+  $result = json_encode(array('status' => 'error' , 'message' => '登録を失敗しました。'));
 }
 $dbConnect->dbClose();
 echo($result);
