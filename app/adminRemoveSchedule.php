06dd3e486f073b11f9e95b1de96e17bfe282d9c5
Wi_armonia <leegosoo11@gmail.com>
2021-11-08 01:42:13 +0900

    adminRemove php recort log

--- app/adminRemoveSchedule.php
+++ app/adminRemoveSchedule.php
@@ -3,6 +3,20 @@ $response = json_decode(file_get_contents('php://input'), true);
 $current_date = $response['current_date'];
 include 'sqlConnect.php';
 try {
+  $rootPath = $_SERVER['DOCUMENT_ROOT'].'/schedule/log/';
+  $time = date('Y/m/d-H:i');
+  $logDate = date('Ymd');
+  $path = $rootPath.$logDate.".txt";
+  if (!file_exists($path)) {
+    $log = @fopen($path,"a+");
+    @fwrite($log,"time, api, name, condition\n");
+    @fclose($log);
+  }
+  $remoteAddr = $_SERVER['REMOTE_ADDR'];
+  $log = @fopen($path,"a+");
+  @fwrite($log,"$time, adminRemove, admin, $current_date\n");
+  @fclose($log);
+
   $dbConnect = new mysqlConnect();
   $del = "DELETE FROM schedule WHERE agenda = '' AND date < '$current_date'";
   $dbConnect->mysql->query($del);
@@ -12,11 +26,6 @@ try {
   $time = date('Y/m/d-H:i');
   $logDate = date('Ymd');
   $path = $rootPath."error_".$logDate.".txt";
-  if($search_condition == true) {
-    $condition = "search";
-  } else {
-    $condition = "";
-  }
   if (!file_exists($path)) {
     $log = @fopen($path,"a+");
     @fwrite($log,"time, api, error\n");
