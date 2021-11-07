5b9b9a97fa34d9bf5277db123245e9d2fd2f6866
Wi_armonia <leegosoo11@gmail.com>
2021-11-08 01:41:27 +0900

    insert new data with admin page

---
+++ app/adminSchedule.php
@@ -0,0 +1,43 @@
+<?php
+$response = json_decode(file_get_contents('php://input'), true);
+$event = $response['event'];
+include 'sqlConnect.php';
+try {
+  $dbConnect = new mysqlConnect();
+  $index = 0;
+  $sql_array = [];
+  if ($event != []) {
+    foreach ($event as $values) {
+      $sql_array[$index] = "( '{$values['name']}', '{$values['date']}', '{$values['agenda']}', '{$values['start_time']}', '{$values['end_time']}', '{$values['total_time']}', 
+                            '{$values['staff_hour_salary']}', '{$values['staff_day_salary']}', '{$values['staff_expense']}', 
+                            '{$values['admin_hour_salary']}', '{$values['admin_day_salary']}', '{$values['admin_expense']}' )";
+      $index++;
+    }
+    $sql = "INSERT INTO schedule ( name, date, agenda, start_time, end_time, total_time, staff_hour_salary, staff_day_salary, staff_expense, admin_hour_salary, admin_day_salary , admin_expense ) VALUES";
+    $sub_sql = "ON DUPLICATE KEY UPDATE agenda = VALUES(agenda), start_time = VALUES(start_time), end_time = VALUES(end_time), total_time = VALUES(total_time), 
+              staff_hour_salary = VALUES(staff_hour_salary), staff_day_salary = VALUES(staff_day_salary),  staff_expense = VALUES(staff_expense),
+              admin_hour_salary = VALUES(admin_hour_salary), admin_day_salary = VALUES(admin_day_salary), admin_expense = VALUES(admin_expense)";
+    $sub_sql_query = implode(', ', $sql_array);
+    $sql = $sql.$sub_sql_query.$sub_sql;
+    $dbConnect->mysql->query($sql);
+    $result = json_encode(array('status' => true));
+  }
+} catch(Exception $e) {
+  $rootPath = $_SERVER['DOCUMENT_ROOT'].'/schedule/log/';
+  $time = date('Y/m/d-H:i');
+  $logDate = date('Ymd');
+  $path = $rootPath."error_".$logDate.".txt";
+  if (!file_exists($path)) {
+    $log = @fopen($path,"a+");
+    @fwrite($log,"time, api, error\n");
+    @fclose($log);
+  }
+  $remoteAddr = $_SERVER['REMOTE_ADDR'];
+  $log = @fopen($path,"a+");
+  @fwrite($log,"$time, adminEditSchedule, $e\n");
+  @fclose($log);
+  $result = json_encode(array('status' => false , 'message' => '登録を失敗しました。'));
+}
+$dbConnect->dbClose();
+echo($result);
+?>
