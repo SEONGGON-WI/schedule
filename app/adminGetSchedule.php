815897cc8c76b0edd406b0184e8186da07088118
Wi_armonia <leegosoo11@gmail.com>
2021-11-12 02:07:16 +0900

    admin get check time make backup

--- app/adminGetSchedule.php
+++ app/adminGetSchedule.php
@@ -6,22 +6,53 @@ include 'sqlConnect.php';
 try {
   $dbConnect = new mysqlConnect();
   $data = $dbConnect->getAdmin($start_date, $end_date);
-
   if (!empty($data)) {
     $result = json_encode(array('status' => true , 'data' => $data));
   } else {
     $result = json_encode(array('status' => true , 'data' => ''));
+  }
+
+  $time = (int)date('H');
+  if ($time < 9 && $time >= 1) {
+    $fileName = "1_backup.json";
+    $deleteName = "22_backup.json";
+  } else if ($time < 18 && $time >= 9) {
+    $fileName = "9_backup.json";
+    $deleteName = "1_backup.json";
+  } else if ($time < 22 && $time >= 18) {
+    $fileName = "18_backup.json";
+    $deleteName = "9_backup.json";
+  } else if ($time >= 22) {
+    $fileName = "22_backup.json";
+    $deleteName = "18_backup.json";
+  } else {
+    $fileName = "22_backup.json";
+    $deleteName = "18_backup.json";
+  }
+
+  $rootPath = $_SERVER['DOCUMENT_ROOT'].'/schedule/log/';
+  $backup_path = $rootPath.$fileName;
+  $delete_path = $rootPath.$deleteName;
+
+  if (!file_exists($backup_path)) {
+    $time = date('Y-m-');
+    $current_start = $time.'1';
+    $current_end = $time.'31';
+    $backup_data = $dbConnect->getAdmin($current_start, $current_end);
+    $json_data = json_encode($backup_data);
+    $log = @fopen($backup_path,"w+");
+    @fwrite($log,"$json_data");
+    @fclose($log);
+
+    if (file_exists($delete_path)) {
+      unlink($delete_path);
+    }
   }
 } catch(Exception $e) {
   $rootPath = $_SERVER['DOCUMENT_ROOT'].'/schedule/log/';
   $time = date('Y/m/d-H:i');
   $logDate = date('Ymd');
   $path = $rootPath."error_".$logDate.".txt";
-  if (!file_exists($path)) {
-    $log = @fopen($path,"a+");
-    @fwrite($log,"time, api, error\n");
-    @fclose($log);
-  }
   $log = @fopen($path,"a+");
   @fwrite($log,"$time, adminGetSchedule, $e\n");
   @fclose($log);
