161b0b225def2eb3595338213f7c4a15d68c32df
Wi_armonia <leegosoo11@gmail.com>
2022-04-16 01:59:06 +0900

    edit client upload logic

--- app/adminUploadSchedule.php
+++ app/adminUploadSchedule.php
@@ -16,14 +16,22 @@ try {
     foreach ($client as $values) {
       $client_sql = "UPDATE schedule SET client = '{$values['client']}' WHERE agenda = '{$values['agenda']}' AND date >= '$start_date' AND date <= '$end_date'";
       $dbConnect->mysql->query($client_sql);
-      $hour_sql = "UPDATE schedule SET admin_hour_salary = '{$values['hour_salary']}' WHERE agenda = '{$values['agenda']}' AND admin_hour_salary != '0' AND date >= '$start_date' AND date <= '$end_date'";
-      $dbConnect->mysql->query($hour_sql);
-      $day_sql = "UPDATE schedule SET admin_day_salary = '{$values['day_salary']}' WHERE agenda = '{$values['agenda']}' AND admin_day_salary != '0' AND date >= '$start_date' AND date <= '$end_date'";
-      $dbConnect->mysql->query($day_sql);
-      $staff_hour_sql = "UPDATE schedule SET staff_hour_salary = '{$values['staff_hour_salary']}' WHERE agenda = '{$values['agenda']}' AND staff_hour_salary != '0' AND date >= '$start_date' AND date <= '$end_date'";
-      $dbConnect->mysql->query($staff_hour_sql);
-      $staff_day_sql = "UPDATE schedule SET staff_day_salary = '{$values['staff_day_salary']}' WHERE agenda = '{$values['agenda']}' AND staff_day_salary != '0' AND date >= '$start_date' AND date <= '$end_date'";
-      $dbConnect->mysql->query($staff_day_sql);
+      if ($values['hour_salary'] != '') {
+        $hour_sql = "UPDATE schedule SET admin_hour_salary = '{$values['hour_salary']}' WHERE agenda = '{$values['agenda']}' AND date >= '$start_date' AND admin_hour_salary == '' AND date <= '$end_date'";
+        $dbConnect->mysql->query($hour_sql);
+      }
+      if ($values['day_salary'] != '') {
+        $day_sql = "UPDATE schedule SET admin_day_salary = '{$values['day_salary']}' WHERE agenda = '{$values['agenda']}' AND date >= '$start_date' AND admin_day_salary == '' AND date <= '$end_date'";
+        $dbConnect->mysql->query($day_sql);  
+      }
+      if ($values['staff_hour_salary'] != '') {
+        $staff_hour_sql = "UPDATE schedule SET staff_hour_salary = '{$values['staff_hour_salary']}' WHERE agenda = '{$values['agenda']}' AND staff_hour_salary == '' AND date >= '$start_date' AND date <= '$end_date'";
+        $dbConnect->mysql->query($staff_hour_sql);  
+      }
+      if ($values['staff_day_salary'] != '') {
+        $staff_day_sql = "UPDATE schedule SET staff_day_salary = '{$values['staff_day_salary']}' WHERE agenda = '{$values['agenda']}' AND staff_day_salary == '' AND date >= '$start_date' AND date <= '$end_date'";
+        $dbConnect->mysql->query($staff_day_sql);  
+      }
     }
     $result = json_encode(array('status' => true));
   } else {
