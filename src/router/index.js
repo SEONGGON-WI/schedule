1ec14a9473dc17e100a507117a83eae260e1f333
Wi_armonia <leegosoo11@gmail.com>
2022-05-07 15:53:05 +0900

    router edit

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ Vue.use(VueRouter)
 
 // import Staff from '../views/Staff.vue'
 // import Admin from '../views/Admin.vue'
-import Master from '@/views/Master.vue'
 
 const routes = [
   {path: '/', redirect: {name: 'staff'}},
@@ -12,9 +11,8 @@ const routes = [
   // {path: '/staff', name: 'staff', component: Staff},
   // {path: '/admin', name: 'admin', component: Admin},
   {path: '/staff', name: 'staff', component: () => import ('@/views/Staff.vue')},
-  {path: '/admin', name: 'staff', component: () => import ('@/views/Staff.vue')},
-  {path: '/management', name: 'admin', component: () => import ('@/views/management.vue')},
-  {path: '/master', name: 'master', component: Master},
+  {path: '/admin', redirect: {name: 'staff'}},
+  {path: '/master', name: 'master', component: () => import ('@/views/master.vue')},
 
   // {path: '/admin', name: 'admin', component: () => import(/* webpackChunkName: "about" */ '../views/Admin.vue')},
 // route level code-splitting
