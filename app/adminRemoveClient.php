351ef8816332d36003dfc527c462e0eb48b59167
Wi_armonia <leegosoo11@gmail.com>
2022-05-29 19:20:37 +0900

    admin remove client reset data

--- app/adminRemoveClient.php
+++ app/adminRemoveClient.php
@@ -15,7 +15,7 @@ try {
   @fwrite($log,"$time, adminRemoveClient, $client, $agenda, $start_date\n");
   @fclose($log);
   $dbConnect = new mysqlConnect();
-  $sql = "UPDATE schedule SET admin_hour_salary = '', admin_day_salary = '', staff_hour_salary = '', staff_day_salary = '', client = '' WHERE agenda = '$agenda' AND date >= '$start_date' AND date <= '$end_date'";
+  $sql = "UPDATE schedule SET admin_hour_salary = '', admin_day_salary = '', staff_hour_salary = '', staff_day_salary = '', client = '', agenda = '', status  = 0, tax_status= 0 WHERE agenda = '$agenda' AND date >= '$start_date' AND date <= '$end_date'";
   $dbConnect->mysql->query($sql);
   $del = "DELETE FROM client WHERE client = '$client' AND agenda = '$agenda' AND date = '$start_date'";
   $dbConnect->mysql->query($del);
